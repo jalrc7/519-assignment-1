8d69a237e2a451f71ecaf9d2d776a84200679d3a
jalrc7 <jalrc@bu.edu>
2024-03-31 23:26:58 -0400

    Refine date functionality in Azure Function

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -1,10 +1,14 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
+    const daysToAdd = parseInt(req.query.daysToAdd || "0", 10);
+    const today = new Date();
+    
+    const formattedDate = today.toISOString().split('T')[0];
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? 'Hello,  ${name} . This HTTP triggered function executed successfully. Todays date is ${formattedDate}'
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
