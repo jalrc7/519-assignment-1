91524c951e5d631a8535f941ccf0f4222d38a29f
jalrc7 <jalrc@bu.edu>
2024-04-02 15:18:54 -0400

    changes to queue processor

--- Node/API2/QueueProcessor/index.js
+++ Node/API2/QueueProcessor/index.js
@@ -1,3 +1,5 @@
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    const secret3 = process.env.myStorageConnectionAppSetting;
+    context.log('secret 3:', secret3);
 };
